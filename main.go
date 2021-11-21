c70a8a0de8206fd802a627279cd9d7ee0a0597b2
mendozg <35119606+mendozg@users.noreply.github.com>
2021-11-21 12:09:21 -0600

    Update main.go

--- main.go
+++ main.go
@@ -6,9 +6,9 @@ import (
 	"os"
 
 	logging "github.com/ipfs/go-log/v2"
-	"github.com/mining-pool/not-only-mining-pool/config"
-	"github.com/mining-pool/not-only-mining-pool/pool"
-	"github.com/mining-pool/not-only-mining-pool/utils"
+	"github.com/mendozg/not-only-mining-pool/config"
+	"github.com/mendozg/not-only-mining-pool/pool"
+	"github.com/mendozg/not-only-mining-pool/utils"
 )
 
 var log = logging.Logger("main")

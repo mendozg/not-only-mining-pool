3280aa7930b68136d77083d8b84582cdbbff4b5a
mendozg <35119606+mendozg@users.noreply.github.com>
2021-11-21 11:33:22 -0600

    Update jobManager.go

--- jobs/jobManager.go
+++ jobs/jobManager.go
@@ -9,12 +9,12 @@ import (
 	"time"
 
 	logging "github.com/ipfs/go-log/v2"
-	"github.com/mining-pool/not-only-mining-pool/algorithm"
-	"github.com/mining-pool/not-only-mining-pool/config"
-	"github.com/mining-pool/not-only-mining-pool/daemons"
-	"github.com/mining-pool/not-only-mining-pool/storage"
-	"github.com/mining-pool/not-only-mining-pool/types"
-	"github.com/mining-pool/not-only-mining-pool/utils"
+	"github.com/mendozg/not-only-mining-pool/algorithm"
+	"github.com/mendozg/not-only-mining-pool/config"
+	"github.com/mendozg/not-only-mining-pool/daemons"
+	"github.com/mendozg/not-only-mining-pool/storage"
+	"github.com/mendozg/not-only-mining-pool/types"
+	"github.com/mendozg/not-only-mining-pool/utils"
 )
 
 var log = logging.Logger("jobMgr")

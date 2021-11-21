7308a01d53d9cb86997e83fe4eb1b9c0b874d4bf
mendozg <35119606+mendozg@users.noreply.github.com>
2021-11-21 11:37:16 -0600

    Update peer.go

--- p2p/peer.go
+++ p2p/peer.go
@@ -10,8 +10,8 @@ import (
 	"time"
 
 	logging "github.com/ipfs/go-log/v2"
-	"github.com/mining-pool/not-only-mining-pool/config"
-	"github.com/mining-pool/not-only-mining-pool/utils"
+	"github.com/mendozg/not-only-mining-pool/config"
+	"github.com/mendozg/not-only-mining-pool/utils"
 )
 
 var log = logging.Logger("p2p")
